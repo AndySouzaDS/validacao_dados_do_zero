82c7059186f7a3883f473f059a06178c42ab54ef
andysouza <andysouzads@gmail.com>
2024-04-14 21:18:34 -0300

    atualizacao do exemplo 01

---
+++ ex_01.py
@@ -0,0 +1,15 @@
+# : str - tipando as variáveis
+nome_produto: str = "Mesa de Escritório"
+quantidade_estoque: int = 10
+preco_unitario: float = 450.0
+disponivel_venda: bool = True
+data_validade = 2024-12-31
+
+# print("Olá mundo!")
+# print(quantidade_estoque)
+# print(f"Meu produto é o: {nome_produto}")
+
+# valor_inserido_pelo_usuario: str = input("Digite um texto aqui: ")
+# print(valor_inserido_pelo_usuario)
+
+print(type(nome_produto))
