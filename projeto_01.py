b639fd1f808c390f6c7f4352afcfb909a6e7a626
andysouza <andysouzads@gmail.com>
2024-04-15 13:38:28 -0300

    atualizar projeto

--- projeto_01.py
+++ projeto_01.py
@@ -1,7 +1,16 @@
 import pandas as pd
 from pandas import DataFrame
+from schema import InventarioSchema
 
-# tipando como DataFrame
+
+# tipando como DataFrame - usar essa importacao: from pandas import DataFrame
 minha_tabela: DataFrame = pd.read_excel("data/inventario_mundo.xlsx")
 
-print(minha_tabela)
+# print(minha_tabela)
+
+try:
+    InventarioSchema.validate(minha_tabela)
+    # print(minha_tabela)
+    print("Excel, validado! Dados consistentes.")
+except Exception as e:
+    print(f"Erros no schema: {e}")
