2c6a90afabe331318030fb74feac5e7404dfecc3
andysouza <andysouzads@gmail.com>
2024-04-14 21:34:20 -0300

    atualizacao do projeto

--- ex_02.py
+++ ex_02.py
@@ -1,6 +1,10 @@
+from datetime import datetime
+
 nome_produto: str = "Mesa de Escritório"
 quantidade_estoque: int = 10
 preco_unitario: float = 450.0
 disponivel_venda: bool = True
-data_validade = 2024-12-31
+data_validade: datetime = datetime(2024, 12, 31)
 
+print(data_validade)
+print(type(data_validade))
